8ed30eb1fec070da27f85d719eb759b70c6110ec
Haranana <hubert.osmalek@op.pl>
2025-12-15 13:06:18 +0100

    added basics of dtos and ipGeoClient

---
+++ Core/Dtos.cs
@@ -0,0 +1,58 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core
+{
+
+    public class AstronomyResponseDto
+    {
+        [JsonProperty("location")]
+        public LocationDto Location { get; set; }
+
+        [JsonProperty("Astronomy")]
+        public AstronomyDto Astronomy { get; set; }
+
+    }
+    public class LocationDto
+    {
+        [JsonProperty("latitude")]
+        public string Latitude { get; set; }
+
+        [JsonProperty("longitude")]
+        public string Longitude { get; set; }
+
+        [JsonProperty("city")]
+        public string City { get; set; }
+
+        [JsonProperty("country_name")]
+        public string CountryName { get; set; }
+    }
+
+    public class AstronomyDto
+    {
+        [JsonProperty("date")]
+        public string Date { get; set; }
+
+        [JsonProperty("sunrise")]
+        public string Sunrise { get; set; }
+
+        [JsonProperty("sunset")]
+        public string Sunset { get; set; }
+
+        [JsonProperty("day_length")]
+        public string DayLength { get; set; }
+
+        [JsonProperty("moon_phase")]
+        public string MoonPhase { get; set; }
+
+        [JsonProperty("moonrise")]
+        public string Moonrise { get; set; }
+
+        [JsonProperty("moonset")]
+        public string Moonset { get; set; }
+    }
+}
---
+++ Core/IpGeoClient.cs
@@ -0,0 +1,64 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Configuration;
+using System.Globalization;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Core
+{
+    public class IpGeoClient
+    {
+
+        private readonly HttpClient HttpClient;
+        private readonly string ApiKey;
+        private readonly string ApiUrl;
+
+        public IpGeoClient(HttpClient httpClient)
+        {
+            HttpClient = httpClient ?? throw new ArgumentNullException("HttpClient is null");
+
+            NameValueCollection AllAppSettings = ConfigurationManager.AppSettings;           
+            ApiUrl = AllAppSettings["IPGEO_URL"];
+            ApiKey = Environment.GetEnvironmentVariable("IPGEO_API_KEY");
+
+            if (string.IsNullOrWhiteSpace(ApiKey))
+                throw new InvalidOperationException("App.config is missing API URL");
+
+            if (string.IsNullOrWhiteSpace(ApiUrl))
+                throw new InvalidOperationException("missing API KEY environment variable");
+        }
+
+        public async Task<AstronomyResponseDto> GetAstronomyAsync(
+            double lat, double lon, DateTime? date = null
+        ){
+            string url = BuildUrl(lat, lon, date);
+
+            using (HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, url))
+            using (HttpResponseMessage response = await HttpClient.SendAsync(request, HttpCompletionOption.ResponseHeadersRead, ct).ConfigureAwait(false))
+            {
+
+                string responseBody = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+
+                if (!response.IsSuccessStatusCode)
+                    throw new HttpRequestException($"API error {response.StatusCode} : {responseBody}");
+
+                return JsonConvert.DeserializeObject<AstronomyResponseDto>(responseBody);
+            }
+        }
+
+        private string BuildUrl(double lat, double lon, DateTime? date)
+        {
+            string url = $"{ApiUrl}/v2/astronomy?apiKey={Uri.EscapeDataString(ApiKey)}&lat={Uri.EscapeDataString(lat.ToString())}&long={Uri.EscapeDataString(lon.ToString())}";
+
+            if (date.HasValue) url += $"&date={date.Value:yyyy-MM-dd}";
+
+            return url;
+        }
+    }
+}
